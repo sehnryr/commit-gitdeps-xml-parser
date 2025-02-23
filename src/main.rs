53bcdb038efd0251552e557ae3c0c3bb85c61f74
Youn Mélois <youn@melois.dev>
2025-02-23 03:47:52 +0100

    move file content fetching into GitDeps

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,9 @@ mod args;
 mod model;
 mod parser;
 
-use async_compression::tokio::bufread::GzipDecoder;
-use clap::Parser as ClapParser;
-use futures_util::TryStreamExt;
+use clap::Parser;
 use tokio::fs::OpenOptions;
 use tokio::io;
-use tokio_util::io::StreamReader;
 
 use crate::args::Args;
 use crate::model::GitDeps;
@@ -26,8 +23,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .join(args.file().file_name().ok_or("File name not found")?);
 
     let file = git_deps.get_file(file_path_str).unwrap();
-
-    let url = git_deps.get_file_url(&file).ok_or("File not found")?;
 
     let mut output_file = OpenOptions::new()
         .write(true)
@@ -49,17 +44,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
     }
 
-    let response = reqwest::get(url).await?;
-    let byte_stream = response
-        .bytes_stream()
-        .map_err(|e| io::Error::new(io::ErrorKind::Other, e));
+    let mut content_reader = git_deps.get_file_content(&file).await?;
 
-    let stream_reader = StreamReader::new(byte_stream);
-    let buf_reader = io::BufReader::new(stream_reader);
-
-    let mut decoder = GzipDecoder::new(buf_reader);
-
-    io::copy(&mut decoder, &mut output_file).await?;
+    io::copy(&mut content_reader, &mut output_file).await?;
 
     Ok(())
 }
--- src/model/git_deps.rs
+++ src/model/git_deps.rs
@@ -1,5 +1,10 @@
+use async_compression::tokio::bufread::GzipDecoder;
+use futures_util::TryStreamExt;
 use nom::Parser;
 use nom::character::complete::multispace0;
+use tokio::io;
+use tokio::io::AsyncRead;
+use tokio_util::io::StreamReader;
 
 use crate::parser::*;
 
@@ -43,5 +48,24 @@ impl<'a> GitDeps<'a> {
             remote_path = pack.remote_path(),
             pack_hash = blob.pack_hash()
         ))
+    }
+
+    pub async fn get_file_content(
+        &self,
+        file: &File<'a>,
+    ) -> Result<impl AsyncRead, Box<dyn std::error::Error>> {
+        let url = self.get_file_url(&file).ok_or("File not found")?;
+
+        let response = reqwest::get(url).await?;
+        let byte_stream = response
+            .bytes_stream()
+            .map_err(|e| io::Error::new(io::ErrorKind::Other, e));
+
+        let stream_reader = StreamReader::new(byte_stream);
+        let buf_reader = io::BufReader::new(stream_reader);
+
+        let decoder = GzipDecoder::new(buf_reader);
+
+        Ok(decoder)
     }
 }
